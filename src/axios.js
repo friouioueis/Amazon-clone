325d97b0f8687bd3744ff1d38fb52f6ee84aa863
unknown <go_frioui@esi.dz>
2021-04-06 17:50:33 +0100

    final commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-d4791/us-central1/api",
+  baseURL: "https://us-central1-clone-d4791.cloudfunctions.net/api",
 });
 
 export default instance;
